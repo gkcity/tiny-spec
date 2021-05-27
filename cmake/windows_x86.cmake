4ede9ecbb0f06f9cb8739762daf0ac548f220c52
jxfengzi <178915185@qq.com>
2021-05-27 15:27:44 +0800

    init source code

---
+++ cmake/windows_x86.cmake
@@ -0,0 +1,41 @@
+MESSAGE(STATUS "make for windows")
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_SYSTEM_PROCESSOR x86)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+#----------------------------------------------------------------------------
+#
+# for Visual Studio C++
+#
+#----------------------------------------------------------------------------
+ADD_DEFINITIONS(-D_WINSOCK_DEPRECATED_NO_WARNINGS)
+ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS)
+ADD_DEFINITIONS(-DUNICODE)
+ADD_DEFINITIONS(-D_UNICODE)
+ADD_DEFINITIONS(-D__WIN32__)
+
+#----------------------------------------------------------------------------
+#
+# Dependence Libraries
+#
+#----------------------------------------------------------------------------
+SET(DEPENDENCE_LIB ws2_32.lib tiny.lib)
+MESSAGE(STATUS "set dependence library: ${DEPENDENCE_LIB}")
+
+#----------------------------------------------------------------------------
+#
+# DEBUG INFORMATION
+#
+#----------------------------------------------------------------------------
+ADD_DEFINITIONS(-DTINY_DEBUG)
+
+#----------------------------------------------------------------------------
+#
+# Export library
+#
+#----------------------------------------------------------------------------
+SET(EXPORT_STATIC_LIBRARY 0)
+SET(EXPORT_SHARED_LIBRARY 1)
